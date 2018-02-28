21907331c53e31d892ad8b7fcece3eec1d176abb
Paul Revelo <prevelo01@manhattan.edu>
2018-02-28 16:02:56 -0500

    Added basic login functionality with firebase.
    Added ability to zoom in on camera feed.

--- app/src/main/java/com/example/student/spicamera/MainActivity.java
+++ app/src/main/java/com/example/student/spicamera/MainActivity.java
@@ -1,29 +1,87 @@
 package com.example.student.spicamera;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     private WebView wv1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //[GOOGLE SIGN IN OBJECTS NEEDED TO LOGOUT]
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //[END GOOGLE]
+
         wv1=(WebView)findViewById(R.id.webView);
         wv1.setWebViewClient(new MyBrowser());
         //IP Adress of the rPi
-        String url = "http://149.61.175.189:8082/";
-
+        String url = "http://spicam:jasper123@149.61.173.138:8082/";
+        wv1.setInitialScale(1);
         wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setLoadWithOverviewMode(true);
+        wv1.getSettings().setUseWideViewPort(true);
         wv1.getSettings().setJavaScriptEnabled(true);
-
-        wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv1.getSettings().setBuiltInZoomControls(true);
+        wv1.getSettings().setDisplayZoomControls(false);
+        wv1.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
         wv1.loadUrl(url);
+
+        Button button = (Button) findViewById(R.id.sign_out_button_cam);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                signOut();
+            }
+        });
+
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //OPEN Sign In Page
+                        goToSignInPage();
+                    }
+                });
+    }
+
+    private void goToSignInPage() {
+        Intent intent = new Intent(this, GoogleSignInActivity.class);
+        startActivity(intent);
     }
 
     private class MyBrowser extends WebViewClient {
