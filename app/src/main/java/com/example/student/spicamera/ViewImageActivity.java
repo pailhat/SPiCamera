0674e7c922465412c7cc78d4c5f74c0ffe2d8da4
Paul Revelo <prevelo01@manhatan.edu>
2018-04-18 17:17:14 -0400

    Final Touches

--- app/src/main/java/com/example/student/spicamera/ViewImageActivity.java
+++ app/src/main/java/com/example/student/spicamera/ViewImageActivity.java
@@ -141,7 +141,7 @@ public class ViewImageActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
         //Change bell icon when notificaiton is received
         intentFilter = new IntentFilter();
